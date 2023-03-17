c38e4d5c61237ff2ca4104a9e18d79436c630e31
Andy29er <andra.luana.ac@gmail.com>
2023-03-18 00:47:26 +0200

    PCM-1 - SETUP HIBERNATE CONFIGURATION

--- src/main/java/practicalprojectexample/Main.java
+++ src/main/java/practicalprojectexample/Main.java
@@ -1,6 +1,12 @@
 package practicalprojectexample;
 
+import practicalprojectexample.utils.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory(); // to force an initialization
+
+
+        SessionManager.shutdown();
     }
 }
